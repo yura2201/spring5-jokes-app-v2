db1c1037a693190dd5259e9e583c015d3bdf92ed
Yuriy Tsarkov <yurait6@gmail.com>
2022-09-01 11:12:45 +0300

    fix controller issues

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -2,15 +2,14 @@ package guru.springframework.spring5jokesappv2.controllers;
 
 import guru.springframework.spring5jokesappv2.services.JokeService;
 
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Yuriy Tsarkov (yurait6@gmail.com) on 01.09.2022
  */
-@RestController
+@Controller
 public class JokeController {
 
   private final JokeService jokeService;
@@ -19,7 +18,7 @@ public class JokeController {
     this.jokeService = jokeService;
   }
 
-  @GetMapping( value = {"/", ""}, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  @GetMapping( value = {"/", ""})
   public String showJoke(Model model) {
     model.addAttribute("joke", jokeService.getJoke());
 
